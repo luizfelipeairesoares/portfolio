a29d1c18811722b967a4c3104f1fed6c34c3bc2c
Luiz Aires Soares <lfelipeas@gmail.com>
2024-12-02 19:36:12 -0500

    Initial Commit

---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by Luiz Aires Soares on 2024-12-02.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ portfolioTests/portfolioTests.swift
@@ -0,0 +1,16 @@
+//
+//  portfolioTests.swift
+//  portfolioTests
+//
+//  Created by Luiz Aires Soares on 2024-12-02.
+//
+
+import Testing
+
+struct portfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
